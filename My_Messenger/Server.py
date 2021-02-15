7e2021a9f9682250793704f32637b43cc6a6b78b
Igor <rick_id@mail.com>
2021-02-15 10:58:47 +0300

    Добавлено логирование.

--- My_Messenger/Server.py
+++ My_Messenger/Server.py
@@ -2,8 +2,12 @@ import argparse
 import time
 from json import JSONDecodeError
 from socket import AF_INET, socket, SOCK_STREAM
+import logging
+import log.Server.server_log_config
 
 from common.utils import send_message, get_data_from_message, load_setting
+
+logger = logging.getLogger('server')
 
 
 def send_success_code(client):
@@ -14,28 +18,34 @@ def send_success_code(client):
     send_message(client, msg_response)
 
 
-SETTINGS = load_setting(is_server=False)
-parser = argparse.ArgumentParser(description='Server arguments')
-parser.add_argument('addr', type=str, nargs='*', default='', help='Clients address')
-parser.add_argument('port', type=int, nargs='*', default='', help='server port')
-args = parser.parse_args()
+def main():
+    SETTINGS = load_setting(is_server=False)
+    parser = argparse.ArgumentParser(description='Server arguments')
+    parser.add_argument('addr', type=str, nargs='*', default='', help='Clients address')
+    parser.add_argument('port', type=int, nargs='*', default='', help='server port')
+    args = parser.parse_args()
 
-if not args.port:
-    server_port = SETTINGS["DEFAULT_PORT"]
-else:
-    server_port = args.port
-s = socket(AF_INET, SOCK_STREAM)
-s.bind((args.addr, server_port))
-s.listen(SETTINGS['MAX_CONNECTION'])
+    if not args.port:
+        server_port = SETTINGS["DEFAULT_PORT"]
+        logger.warning("Успользуются порт сервера по умолчанию")
+    else:
+        server_port = args.port
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind((args.addr, server_port))
+    s.listen(SETTINGS['MAX_CONNECTION'])
 
-while True:
-    client, addr = s.accept()
-    try:
-        data = get_data_from_message(client.recv(SETTINGS['MAX_PACKAGE_LENGTH']))
-        print('Сообщение: ', data, ', было отправлено клиентом: ', addr)
-    except JSONDecodeError:
-        print('Некорректный формат JSON файла')
+    while True:
+        client, addr = s.accept()
+        try:
+            data = get_data_from_message(client.recv(SETTINGS['MAX_PACKAGE_LENGTH']))
+            logger.debug(f'Сообщение: {data}, было отправлено клиентом: {addr}')
+        except JSONDecodeError:
+            logger.exception('Некорректный формат JSON файла')
 
-    send_success_code(client)
+        send_success_code(client)
 
-    client.close()
+        client.close()
+
+
+if __name__ == '__main__':
+    main()
--- My_Messenger/common/utils.py
+++ My_Messenger/common/utils.py
@@ -1,24 +1,36 @@
 import json
 import os
 import sys
+import logging
 
 
-def get_data_from_message(response):
-    print('это респонс ', response)
+def get_logger(is_server):
+    if is_server:
+        return logging.getLogger('server')
+    else:
+        return logging.getLogger('client')
+
+
+def get_data_from_message(response, is_server=True):
+    logger = get_logger(is_server)
     response_str = response.decode('utf-8')
+    logger.debug(f'это респонс {response_str}')
     return json.loads(response_str)
 
 
-def send_message(socket, data_dict):
+def send_message(socket, data_dict, is_server=True):
+    logger = get_logger(is_server)
     if isinstance(data_dict, dict):
         data = json.dumps(data_dict)
-        print('сообщение отправлеено')
+        logger.debug(f'Сообщение отправлено  {socket}')
         return socket.send(bytes(data, encoding="utf-8"))
     else:
+        logger.critical("Некорректный формат сообщения")
         raise TypeError
 
 
 def load_setting(is_server=True, filename='settings.json'):
+    logger = get_logger(is_server)
     config_keys = ["DEFAULT_IP_ADDRESS", "DEFAULT_PORT", "MAX_CONNECTION", "MAX_PACKAGE_LENGTH", "USER"]
     if not is_server:
         config_keys.append("DEFAULT_IP_ADDRESS")
@@ -26,7 +38,7 @@ def load_setting(is_server=True, filename='settings.json'):
         configs = json.load(file)
     for key in config_keys:
         if key not in configs:
-            print(f"В конфигурации отсутсвует ключ: {key}")
+            logger.critical(f"В конфигурации отсутсвует ключ: {key}")
             raise ValueError
     return configs
 
